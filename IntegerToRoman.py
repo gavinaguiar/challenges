499051ae8ee7479b1bb27e42fdff08cec13d19cd
Gavin Aguiar <gavin.aguiar@netscout.com>
2018-04-24 16:58:07 -0500

    Minor edits

--- IntegerToRoman.py
+++ IntegerToRoman.py
@@ -1,14 +1,5 @@
-from test.support import temp_cwd
-symbols = {1 : "I", 
-           5 : "V",
-           10 : "X",
-           50 : "L",
-           100 : "C",
-           500 : "D",
-           1000 : "M"}
 
-
-def intToRoman(num, ret = ""):
+def intToRoman(num):
     strs = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
     nums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]        
     ret = ""        
@@ -19,4 +10,4 @@ def intToRoman(num, ret = ""):
             if num == 0:
                 return ret
 
-print(intToRoman(9))
+print(intToRoman(13))
